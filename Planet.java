07f74a55e8aa5b51dd660fb94f75ed4ff748ea8c
RhysWoods <beldair31@gmail.com>
2023-10-29 17:47:11 +0000

    Added variables and a constructor

--- Planet.java
+++ Planet.java
@@ -1,5 +1,19 @@
-public class Planet{
+public class Planet {
 
+    private String name;
+    private int diameter;
+    private int distanceFromSun;
+    private int orbitalPeriod;
+    private int rotationPeriod;
+    private int numberOfMoons;
 
-    
+    public Planet(String name, int diameter, int distanceFromSun, int orbitalPeriod, int rotationPeriod, int numberOfMoons) {
+        this.name = name;
+        this.diameter = diameter;
+        this.distanceFromSun = distanceFromSun;
+        this.orbitalPeriod = orbitalPeriod;
+        this.rotationPeriod = rotationPeriod;
+        this.numberOfMoons = numberOfMoons;
+    }
+
 }
