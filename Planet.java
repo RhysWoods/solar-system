47d73a9ae3e54312a71f2d2f2affa27b1551a48e
RhysWoods <beldair31@gmail.com>
2023-11-02 19:08:01 +0000

    Revised the planet class variables

--- Planet.java
+++ Planet.java
@@ -1,78 +1,56 @@
 public class Planet {
 
     private String name;
-    private double diameter;
-    private double distanceFromSun;
+    private double distance;
     private double angle;
+    private double diameter;
     private String col;
-    private double centreOfRotationAngle;
-    private double centreOfRotationDistance;
 
-    public Planet(String name, double diameter, double distanceFromSun, long orbitalPeriod, double rotationPeriod) {
+    public Planet(String name, double distance, double angle, double diameter, String col) {
         this.name = name;
-        this.diameter = diameter;
-        this.distanceFromSun = distanceFromSun;
+        this.distance = distance;
         this.angle = angle;
-        this.col = col;
-        this.centreOfRotationAngle = centreOfRotationAngle;
-        this.centreOfRotationDistance = centreOfRotationDistance;
-    }
-
-    // Setters
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDiameter(double diameter) {
         this.diameter = diameter;
-    }
-
-    public void setDistanceFromSun(double distanceFromSun) {
-        this.distanceFromSun = distanceFromSun;
-    }
-
-    public void setAngle(double angle) {
-        this.angle = angle;
-    }
-
-    public void setCol(String col) {
         this.col = col;
     }
 
-    public void setCentreOfRotationAngle(double centreOfRotationAngle) {
-        this.centreOfRotationAngle = centreOfRotationAngle;
-    }
-
-    public void setCentreOfRotationDistance(double centreOfRotationDistance) {
-        this.centreOfRotationDistance = centreOfRotationDistance;
-    }
-
-    // Getters
     public String getName() {
         return name;
     }
 
-    public double getDiameter() {
-        return diameter;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public double getDistanceFromSun() {
-        return distanceFromSun;
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
     }
 
     public double getAngle() {
         return angle;
     }
 
+    public void setAngle(double angle) {
+        this.angle = angle;
+    }
+
+    public double getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(double diameter) {
+        this.diameter = diameter;
+    }
+
     public String getCol() {
         return col;
     }
 
-    public double getCentreOfRotationAngle() {
-        return centreOfRotationAngle;
-    }
-
-    public double getCentreOfRotationDistance() {
-        return centreOfRotationDistance;
+    public void setCol(String col) {
+        this.col = col;
     }
 }
